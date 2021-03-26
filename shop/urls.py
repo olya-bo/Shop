b7a890322e1f8574be601fff4e531bf1477faee3
Olha <mycleverowl@gmail.com>
2021-03-26 09:21:48 +0200

    added view for product, list view for returns

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from shop.views import ProductListView, ReturnListView, ProductCreate
+
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="home.html"), name='home')
+    path("", TemplateView.as_view(template_name="home.html"), name='home'),
+    path("products/", ProductListView.as_view(), name="product-list"),
+    path("products/add/", ProductCreate.as_view(), name="product-create"),
+    path("returns/", ReturnListView.as_view(), name="return-list"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
-# Create your views here.
+from shop.models import Product, Return
+
+
+class ProductListView(ListView):
+    model = Product
+    paginate_by = 100
+
+
+class ReturnListView(ListView):
+    model = Return
+    paginate_by = 100
+
+
+class ProductCreate(CreateView):
+    model = Product
+    fields = ['name', "description", 'price', 'image', ]
+    success_url = reverse_lazy("product-list")
